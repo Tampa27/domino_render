231bc095c99e1e84211091df4484e069324ae991
Ahmed <ahmedlp9@gmail.com>
2024-06-04 11:53:58 -0400

    fix sintax error

--- dominoapp/urls.py
+++ dominoapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('players/', views.PlayerView.as_view(), name='players')
-    path('players/<int:id>/', views.PlayerView.as_view()),  
-    path('players/<int:id>/update/',views.PlayerView.as_view())
+    path('players/', views.PlayerView.as_view(), name='players'),
+    path('players/<int:id>/', views.PlayerView.as_view()),
+    path('players/<int:id>/update/',views.PlayerView.as_view()),
 ]
