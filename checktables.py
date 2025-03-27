36e57da999837141974d9fbc8fa292433ca83888
Ahmed <ahmedlp9@gmail.com>
2025-03-27 16:05:56 -0400

    add script

--- checktables.py
+++ checktables.py
@@ -14,29 +14,24 @@ import sys
 import os
 import django
 
-
-sys.path.append('/home/ahmedlp9/domino_render')  # Ruta absoluta a tu proyecto
+# Configurar entorno Django
+PROJECT_ROOT = '/home/ahmedlp9/domino_render'
+sys.path.append(PROJECT_ROOT)  # Ruta absoluta a tu proyecto
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'domino.settings')
 django.setup()
 
 from dominoapp import views
 from dominoapp.models import Player
-
-# Configurar entorno Django
-
 # Importar tus módulos después de configurar Django
 #from tu_app import tasks  # Ejemplo de importación
 
 def main():
     while True:
-        try:
-            # Ejecutar tus tareas
-            #tasks.mi_tarea()  # Ejemplo de llamada a otro archivo
-            print("Tarea ejecutada correctamente")
-        except Exception as e:
-            print(f"Error: {str(e)}")
+        player = Player.objects.get(alias="mariocondepr")
+        player.coins-=1
+        player.save()
+        time.sleep(10)
         
-        time.sleep(10)  # Esperar 5 segundos
 
 if __name__ == "__main__":
     main()      
