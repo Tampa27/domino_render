77dbfda6222c105da031012042e6a93c52cdf046
Ahmed <ahmedlp9@gmail.com>
2025-03-27 13:39:10 -0400

    add script

---
+++ dominoapp/management/commands/checktables.py
@@ -0,0 +1,16 @@
+import time
+import sys
+import os
+from dominoapp import views
+from dominoapp.models import Player
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        while True:
+            player = Player.objects.get(alias="mariocondepr")
+            player.coins-=1
+            player.save()
+            time.sleep(10)
+        return super().handle(*args, **options)
+      
