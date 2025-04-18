d11d392bd13c447f1c82b88e6947266f6043bc63
mcr92 <manuelcruzrodriguez92@gmail.com>
2025-04-18 11:14:48 -0400

    activate domino api

--- domino/urls.py
+++ domino/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('v2/api/',include('dominoapp.urls')),
+    path('api/',include('dominoapp.urls')),
 
 ]
