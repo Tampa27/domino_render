01ff4058ac4e5c0cfd879e6fe30e4493e9890a7a
mcr92 <manuelcruzrodriguez92@gmail.com>
2025-06-17 17:42:28 -0400

    ajustando el automatico para reducir memoria

--- dominoapp/tasks.py
+++ dominoapp/tasks.py
@@ -1,16 +1,11 @@
 from celery import shared_task
-from datetime import datetime
 import logging
 from dominoapp.utils.checktables import automatic_move_in_game
 logger = logging.getLogger(__name__)
 
 @shared_task
 def automatic_move():
-    hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[Celery] Hora actual: {hora_actual}")
-    logger.info(f"[Celery] Hora actual: {hora_actual}")
+    
     automatic_move_in_game()
-    hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[Celery] Automatic Finish: {hora_actual}")
-    logger.info(f"[Celery] Automatic Finish: {hora_actual}")
-    return hora_actual
+    
+    return
