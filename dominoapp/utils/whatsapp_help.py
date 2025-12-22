e1843f63494c015185f9725e4a160d3a60463a2f
Manuel Cruz Rodriguez <manuelcruzrodriguez92@gmail.com>
2025-12-22 16:28:54 -0500

    actualizar mensajes de whatsap

--- dominoapp/utils/whatsapp_help.py
+++ dominoapp/utils/whatsapp_help.py
@@ -1,16 +1,28 @@
 import urllib.parse
 from dominoapp.models import Player
 
-def get_whatsapp_reload_text(player: Player, amount: int, transaction_id:str, player_phone:str):
+def get_whatsapp_reload_text(player: Player, amount: int, transaction_id:str, player_phone:str, paymentmethod:str=None):
     texto_original = f"""
     Hola *{player.name}*,
                 
 Tu solicitud de recarga para la cuenta **{player.alias}** en Domino Club por un monto de {amount} pesos ha sido recibida con éxito.
-                
+    """
+    if paymentmethod:
+        if paymentmethod == "saldo":
+            method = "📱 Saldo Móvil"
+        else:
+            method = "💳 Transferencia Bancaria"
+            
+        texto_original += f"""
+    Metodo de Pago: {method}
+    """
+    else:
+        texto_original += f"""
 Por favor, elige tu método de pago:
     📱 Saldo Móvil
     💳 Transferencia Bancaria
-
+    """
+    texto_original += f"""
 **ID de tu solicitud**: {transaction_id}
     """
     
