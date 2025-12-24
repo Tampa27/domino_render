18e6a847e40a07392492b096f8a9444462bc9048
Manuel Cruz Rodriguez <manuelcruzrodriguez92@gmail.com>
2025-12-24 14:31:21 -0500

    agregar el numero de match a ganar en la creacion de los torneos

--- dominoapp/serializers.py
+++ dominoapp/serializers.py
@@ -228,7 +228,7 @@ class TournamentCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tournament
-        fields = ["variant", "maxScore", "inPairs", "startWinner",  "moveTime", "min_player", "max_player", "active", "registration_fee", "deadline", "start_at", "winner_payout", "second_payout", "third_payout"]
+        fields = ["variant", "maxScore", "inPairs", "startWinner",  "moveTime", "min_player", "max_player", "active", "registration_fee", "deadline", "start_at", "winner_payout", "second_payout", "third_payout", "number_match_win"]
 
 class MyPlayerSerializer(serializers.ModelSerializer):
 
