08bd0757404e47c0707eae9edfffbdd0872b6934
Manuel Cruz <manuelcruzrodirguez92@gmail.com>
2026-04-17 07:03:06 -0400

    error en el middlewer de los ws

--- domino/asgi.py
+++ domino/asgi.py
@@ -8,7 +8,18 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+# PRIMERO configuramos el entorno
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'domino.settings')
+# SEGUNDO inicializamos Django
+django.setup()
+
+# TERCERO obtenemos la app HTTP (esto debe ir antes de importar routing o middleware)
+django_asgi_app = get_asgi_application()
+
+# CUARTO: Ahora sí, importaciones que dependen de Django/Apps
 from channels.routing import ProtocolTypeRouter, URLRouter
 from dominoapp.custom_middleware import JwtAuthMiddleware
 import dominoapp.routing # Aquí definirás tus rutas de WS
