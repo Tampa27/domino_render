40d2215007f0d82a181a634f42c18c9a7ce3d8ba
Manuel Cruz <manuelcruzrodirguez92@gmail.com>
2026-04-17 06:53:09 -0400

    envolver en la clase y no en el metodo

--- domino/asgi.py
+++ domino/asgi.py
@@ -10,14 +10,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from dominoapp.custom_middleware import JwtAuthMiddlewareStack
+from dominoapp.custom_middleware import JwtAuthMiddleware
 import dominoapp.routing # Aquí definirás tus rutas de WS
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'domino.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": JwtAuthMiddlewareStack( # <--- Envolvemos las rutas aquí
+    "websocket": JwtAuthMiddleware( # <--- Envolvemos las rutas aquí
         URLRouter(
             dominoapp.routing.websocket_urlpatterns
         )
