43b2e4238b0fad215325223b3462d971e25c3611
Manuel Cruz <manuelcruzrodirguez92@gmail.com>
2026-04-20 13:03:15 -0400

    enviar notificacion de torneo abierto

--- dominoapp/management/commands/tournament_automatic.py
+++ dominoapp/management/commands/tournament_automatic.py
@@ -1,10 +1,12 @@
+import os
 import pytz
 from django.core.management.base import BaseCommand
 from django.utils.timezone import timedelta
 from django.utils import timezone
-from dominoapp.models import Tournament
+from dominoapp.models import Tournament, Player
 from dominoapp.tasks import async_send_fcm_message, async_send_global_fcm_message
 from dominoapp.utils.async_task_helper import safe_async_task
+from dominoapp.utils.constants import TournamentStatus
 import logging
 logger = logging.getLogger("django")
 
@@ -17,6 +19,15 @@ class Command(BaseCommand):
             'player_list__user'           # Para notificaciones a todos los inscritos
         )
         now = timezone.now()
+        current_hour = now.hour
+        current_minute = now.minute
+        tournament_notify_hour = os.getenv("TOURNAMENT_NOTIFY_HOURS", None)
+        try:
+            target_hour = int(tournament_notify_hour)
+        except (TypeError, ValueError):
+            target_hour = None
+        if not target_hour:
+            logger.critical(f"No se ha configurado el TOURNAMENT_NOTIFY_HOURS en las variables de entorno.")
         for tournament in tournaments:
             # analizar si el numero de player es par            
             player_list = tournament.player_list.all()
@@ -106,3 +117,30 @@ class Command(BaseCommand):
                 tournament.notification_30 = True
                 tournament.save(update_fields=['notification_30'])
             
+            if tournament.status == TournamentStatus.WAITING_PLAYERS[0] and player_in_tournament < tournament.max_player and  target_hour is not None and current_hour == target_hour and 0 <= current_minute < 10:
+                free_places = tournament.max_player - player_in_tournament
+                player_out_tournament = Player.objects.filter(send_game_notifications = True).exclude(id__in=player_list.values_list('id', flat=True)).order_by("-lastTimeInSystem")
+                if player_out_tournament.exists():
+                    if free_places == 1:
+                        players_to_notify_qs = player_out_tournament[:4]
+                    else:
+                        # Tomamos los más recientes y algunos aleatorios para completar
+                        top_players_ids = list(player_out_tournament[:free_places].values_list('id', flat=True))
+                        others_ids = list(player_out_tournament.exclude(id__in=top_players_ids).order_by("?")[:free_places].values_list('id', flat=True))
+                        all_selected_ids = top_players_ids + others_ids
+                        players_to_notify_qs = Player.objects.filter(id__in=all_selected_ids)
+                    
+                    timezone_list = players_to_notify_qs.order_by("timezone").distinct("timezone").values_list("timezone", flat=True)
+                    for player_timezone in timezone_list:
+                        target_players_ids = players_to_notify_qs.filter(timezone=player_timezone).values_list("user__id", flat=True)
+                        if target_players_ids.exists():
+                            fecha_cierre = tournament.deadline.astimezone(pytz.timezone(player_timezone)).strftime('%d-%m a las %H:%M')
+                            try:
+                                safe_async_task(
+                                    async_send_fcm_message,
+                                    list(target_players_ids),
+                                    "🏆 ¡Torneo de Dominó Abierto!",
+                                    f"Inscríbete ya y compite por la victoria. El registro cierra el {fecha_cierre}. ¡Te esperamos!"
+                                )
+                            except Exception as error:
+                                logger.error(f'Error al enviar notificacion FCM de recordatorio de torneo abierto" => {str(error)}')
