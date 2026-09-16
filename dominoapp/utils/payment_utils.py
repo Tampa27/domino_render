97fc30b7bde120711a7578f9f458cb088d771c82
Manuel Cruz <manuelcruzrodirguez92@gmail.com>
2026-09-16 13:16:28 -0400

    error en las monedas por paises

--- dominoapp/utils/payment_utils.py
+++ dominoapp/utils/payment_utils.py
@@ -60,10 +60,11 @@ def validate_promotion_movie(player: Player):
 
 def get_coins_by_country(player: Player)-> int:
     coins = os.getenv("COINS_BY_MARKETIN", 0)
-    countries_1 = os.getenv("COUNTRIES_1", 0)
-    countries_2 = os.getenv("COUNTRIES_2", 0)
-    if player.country in countries_1:
+    countries_1 = (os.getenv("COUNTRIES_1", '')).split(",")
+    countries_2 = (os.getenv("COUNTRIES_2", '')).split(",")
+
+    if player.country.upper() in countries_1:
         coins = os.getenv("COINS_BY_MARKETIN_COUNTRIES_1", 0)
-    elif player.country in countries_2:
+    elif player.country.upper() in countries_2:
         coins = os.getenv("COINS_BY_MARKETIN_COUNTRIES_2", 0)
     return coins
